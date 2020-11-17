a0e708e7d8a85fbaba2f569f519320663ebbfa02
BBaoVanC <github@bbaovanc.com>
2020-11-17 17:03:29 -0600

    Set up nord theme

--- config.h
+++ config.h
@@ -94,37 +94,37 @@ char *termname = "st-256color";
 unsigned int tabspaces = 8;
 
 /* bg opacity */
-float alpha = 0.8;
+float alpha = 1.0;
 
 /* Terminal colors (16 first used in escape sequence) */
 static const char *colorname[] = {
 	/* 8 normal colors */
-	"black",
-	"red3",
-	"green3",
-	"yellow3",
-	"blue2",
-	"magenta3",
-	"cyan3",
-	"gray90",
+	"#3b4252", // black	(nord1)
+	"#bf616a", // red	(nord11)
+	"#a3be8c", // green	(nord14)
+	"#ebcb8b", // yellow	(nord13)
+	"#81a1c1", // blue	(nord9)
+	"#b48ead", // magenta	(nord15)
+	"#88c0d0", // cyan	(nord8)
+	"#e5e9f0", // white	(nord5)
 
 	/* 8 bright colors */
-	"gray50",
-	"red",
-	"green",
-	"yellow",
-	"#5c5cff",
-	"magenta",
-	"cyan",
-	"white",
+	"#4c566a", // black	(nord3)
+	"#bf616a", // red	(nord11)
+	"#a3be8c", // green	(nord14)
+	"#ebcb8b", // yellow	(nord13)
+	"#81a1c1", // blue	(nord9)
+	"#b48ead", // magenta	(nord15)
+	"#8fbcbb", // cyan	(nord7)
+	"#eceff4", // white	(nord6)
 
 	[255] = 0,
 
 	/* more colors can be added after 255 to use with DefaultXX */
 	"#cccccc", /* 256 -> cursor */
 	"#555555", /* 257 -> rev cursor */
-	"#000000", /* 258 -> bg */
-	"black", /* 259 -> fg */
+	"#2e3440", /* 258 -> bg (nord0) */
+	"#d8dee9", /* 259 -> fg (nord4) */
 };
 
 
@@ -132,10 +132,10 @@ static const char *colorname[] = {
  * Default colors (colorname index)
  * foreground, background, cursor, reverse cursor
  */
-unsigned int defaultfg = 7;
+unsigned int defaultfg = 259;
 unsigned int defaultbg = 258;
-static unsigned int defaultcs = 256;
-static unsigned int defaultrcs = 257;
+static unsigned int defaultcs = 259;
+static unsigned int defaultrcs = 258;
 
 /*
  * Default shape of cursor
